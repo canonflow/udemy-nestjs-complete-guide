cf89d255d8b80113fa3f8235ec1b87f0bb9ea9e9
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-05-17 13:15:35 +0700

    Initial commit

---
+++ typescript/features/annotations/variables.ts
@@ -0,0 +1,28 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let noting: undefined = undefined;
+
+// Built in Objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3, 4, 5];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number, y: number } = {
+    x: 10,
+    y: 20
+};
+
+
+
+
---
+++ typescript/features/types.ts
@@ -0,0 +1,11 @@
+const today = new Date();
+today.getMonth();
+
+// Object
+const person = {
+    age: 20,
+}
+
+// Class
+class Color {}
+const color = new Color();
