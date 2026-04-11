12686fd17eb91617c4b05c1379846eb8abc1da39
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2026-04-11 21:44:48 +0700

    feat(05): read and write to a storage file

--- messages/src/messages/message.repository.ts
+++ messages/src/messages/message.repository.ts
@@ -1,11 +1,27 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 
 export class MessageRepository {
   async findOne(id: string) {
     const content = await readFile('messages.json', 'utf8');
-    const message = JSON.parse(content);
-    return message[id];
+    const messages = JSON.parse(content);
+    return messages[id];
   }
-  async findAll() {}
-  async create(message: string) {}
+
+  async findAll() {
+    const content = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(content);
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+
+    return messages[id];
+  }
 }
